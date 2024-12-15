f62d151d31044fc450a3f8e2d828b17154a815ee
Raulleao <raulleaochagas@gmail.com>
2024-12-15 00:28:19 -0400

    feat: create page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,22 @@
+import React from "react";
 import { View, Text } from "react-native";
 
 export default function Index() {
   return (
-    <View>
-      <Text>Hello React Nativeeeee</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 23,
+        }}
+      >
+        Hello React Nativeeeee
+      </Text>
     </View>
   );
 }
