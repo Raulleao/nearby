901880172c8b2b5254c9936f70fc613f0dc8b9bc
Raulleao <raulleaochagas@gmail.com>
2024-12-30 20:20:44 -0400

    feat: add icons

--- src/components/step/index.tsx
+++ src/components/step/index.tsx
@@ -1,15 +1,19 @@
 import { View, Text } from "react-native";
 import { s } from "./styles";
 import React from "react";
+import { IconProps } from "@tabler/icons-react-native";
+import { colors } from "@/styles/colors";
 
 type Props = {
   title: string;
   description: string;
+  icon: React.ComponentType<IconProps>;
 };
 
-export function Step({ title, description }: Props) {
+export function Step({ title, description, icon: Icon }: Props) {
   return (
     <View style={s.container}>
+      {Icon && <Icon size={32} color={colors.red.base} />}
       <View style={s.datails}>
         <Text style={s.title}>{title}</Text>
         <Text style={s.description}>{description}</Text>
